c69dd63570e3ca065eba7e471d618447e55a21c5
legendongary <legendongary@gmail.com>
2018-09-26 17:45:39 +0800

    Update gen_train.m

--- data/gen_train.m
+++ data/gen_train.m
@@ -6,7 +6,7 @@ for n=1:400
     data{n}.clean = double(imread(sprintf('./train-images/test_%03d.png', n)));
     data{n}.noisy = data{n}.clean + sigma * randn(size(data{n}.clean));
     resim = BM3D(1, data{n}.noisy, sigma);
-    data{n}.index = FAST_NL_Patches(resim, 3, 12, 8, Mindex);
+    data{n}.index = fast_nl_patches(resim, 3, 12, 8, Mindex);
 end
 save(sprintf('./train-sigma-%02d.mat', sigma), 'data');
 end
